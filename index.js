42427decc46bdb00a1d57b4b7db0052d2d9f2f33
alejo <alejoatria@gmail.com>
2022-10-12 16:43:20 -0300

    manejo de params en url

--- index.js
+++ index.js
@@ -11,5 +11,29 @@ app.post('/user', (req, res) => {
     res.send(`Se creó el usuario ${data.name}, que es ${data.descricion}`)
 })
 
+//MANEJAR EL USO DE PARAMS
+//UN PARAMETRO
+app.get('/hello/:name', (req, res) => {
+    let data = req.params
+
+    res.send(`Hola ${data.name}`)
+})
+
+app.get('/hello/:name', (req, res) => {
+    let data = req.params
+
+    res.send(`Hola ${data.name}`)
+})
+
+//MULTIPLES PARAMETROS
+app.get('/add/:x/:y', (req, res) => { //LOS PARAMS PUEDEN ESTAR SEPARADOS POR OTRAS RUTAS .../add/:x/otro/:y 
+    const { x, y } = req.params
+
+    const result = parseInt(x) + parseInt(y)
+
+    res.send(`Resultado es: ${result}`)
+
+})
+
 app.listen(3000)
 console.log(`Server on port ${3000}`)
